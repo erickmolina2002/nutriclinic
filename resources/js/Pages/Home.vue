dcfa9877c26936657d2fed1ee449ef1a1cd2f308
Erick Molina <erickmolina2002@hotmail.com>
2024-01-06 13:28:53 -0300

    chore: configuration

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello world</h1>
+    </div>
+</template>
+<script>
+import { Link } from '@inertiajs/vue3';
+
+export default {
+    components: { Link }
+}
+</script>
+<style>
+    
+</style>
